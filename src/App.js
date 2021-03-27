1f669944e5b2438da59fb236e300946facc243b8
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-03-28 00:20:46 +0200

    Designe Create Note Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import { purple } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+})
 
 function App() {
   return (
-    <Router>
+    <ThemeProvider theme={theme}>
+      <Router>
       <Switch>
         <Route exact path="/">
           <Notes />
@@ -13,7 +32,8 @@ function App() {
           <Create />
         </Route>
       </Switch>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { FormControlLabel, makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  const [category, setCategory] = useState('todos')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setTitleError(false)
+    setDetailsError(false)
+
+    if(title === ''){
+      setTitleError(true)
+    } 
+    
+    if(details === ''){
+      setDetailsError(true)
+    }
+
+    if(title && details){
+      console.log(title, details, category);
+ 
+    } 
+  }
+
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography 
+      variant="h6" 
+      color="textSecondary"
+      component="h2"
+      gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField  
+          onChange={e => setTitle(e.target.value)}
+          className={classes.field}
+            label="Note Title"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+            error={titleError}
+          />
+
+          <TextField  
+          onChange={e => setDetails(e.target.value)}
+          className={classes.field}
+            label="Details"
+            variant="outlined"
+            color="secondary"
+            multiline
+            rows={4}
+            fullWidth
+            required
+            error={detailsError}
+          />
+
+      <FormControl className={classes.field}>
+        <FormLabel>Note Category</FormLabel>
+          <RadioGroup value={category} onChange={e => setCategory(e.target.value)}>
+              <FormControlLabel value="money" control={<Radio />} label="Money" />
+              <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+              <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+              <FormControlLabel value="work" control={<Radio />} label="Work" />
+            </RadioGroup>
+        </FormControl>
+
+      <Button 
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+      </Button>
+      </form>
+
+  
+    </Container>
   )
 }
