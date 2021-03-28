9f0d0a9acc12608d4c41a3fab7691d9078a11dad
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-03-28 19:54:06 +0200

    Implemented post method

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -9,6 +9,7 @@ import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles({
   field: {
@@ -20,6 +21,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles()
+  const history = useHistory()
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
   const [titleError, setTitleError] = useState(false)
@@ -41,7 +43,11 @@ export default function Create() {
     }
 
     if(title && details){
-      console.log(title, details, category);
+      fetch('http://localhost:8000/notes',{
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({title, details, category})
+      }).then(() => history.push('/'))
  
     } 
   }
